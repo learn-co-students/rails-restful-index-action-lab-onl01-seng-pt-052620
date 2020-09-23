7e15aaa1a13ef14c066e6396e8e075118112c1c2
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-09-23 02:22:57 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+Rails.application.routes.draw do 
+  resources :students, only: :index 
+end 
+
+
+
+# Rails.application.routes.draw do
+#   get '/students', to: 'static#students'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
+
